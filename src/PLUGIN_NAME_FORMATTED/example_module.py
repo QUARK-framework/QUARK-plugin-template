03218ddf40a36338704b1cdc87f1a03011c29428
Benjamin Decker <39363928+BenjaminDecker@users.noreply.github.com>
2025-05-08 15:12:40 +0200

    Update example_module.py

--- src/PLUGIN_NAME_FORMATTED/example_module.py
+++ src/PLUGIN_NAME_FORMATTED/example_module.py
@@ -8,8 +8,8 @@ class ExampleModule(Core):
     """
     This is an example module following the recommended structure for a quark module.
 
-    Each module must have a preprocess and postprocess method, as required by the Core protocol.
-    The module interface is defined by the type of data parameter those methods receive and return, dictating which other modules it can be connected to.
+    A module must have a preprocess and postprocess method, as required by the Core abstract base class.
+    A module's interface is defined by the type of data parameter those methods receive and return, dictating which other modules it can be connected to.
     Types defining interfaces should be chosen form QUARKs predefined set of types to ensure compatibility with other modules. TODO: insert link
     """
 
